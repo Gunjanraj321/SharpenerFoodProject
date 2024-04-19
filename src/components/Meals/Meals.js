f9dabe43cb41446aa7762f813be136f0fe6560c3
gunjanraj321 <gunjanraj321@gmail.com>
2024-04-20 00:08:42 +0530

    Meal section Added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./mealSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
